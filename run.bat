4d7d6938c743b215c18db29b15ac826032952db8
Ali Aljawad <alialjawad2k1@gmail.com>
2024-07-08 10:47:43 +0300

    Updated submodules

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ REM Navigate to backend-repo and start the server
 cd backend-repo
 start cmd /k "python server.py"
 
-REM Navigate to frontend-repo and start the client
+REM Naate to frontend-repo and start the client
 cd ../frontend-repo
 start cmd /k "python client.py"
 
